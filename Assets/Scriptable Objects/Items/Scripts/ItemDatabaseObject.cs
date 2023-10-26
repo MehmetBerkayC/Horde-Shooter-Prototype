9ed4a091b8b9e176dd0dfa49c46ceaf1c42811f1
Mehmet Berkay Coruk <89832806+MehmetBerkayC@users.noreply.github.com>
2023-10-26 19:18:29 +0300

    Changes on Database

--- Assets/Scriptable Objects/Items/Scripts/ItemDatabaseObject.cs
+++ Assets/Scriptable Objects/Items/Scripts/ItemDatabaseObject.cs
@@ -6,22 +6,21 @@ using UnityEngine;
 public class ItemDatabaseObject : ScriptableObject, ISerializationCallbackReceiver
 {
     public ItemData[] Items;
-    public Dictionary<ItemData, int> GetID = new Dictionary<ItemData, int>();
     public Dictionary<int, ItemData> GetItem = new Dictionary<int, ItemData>();
 
     public void OnAfterDeserialize()
     {
-        GetID = new Dictionary<ItemData, int>();
         GetItem = new Dictionary<int, ItemData>();
 
         for (int i = 0; i < Items.Length; i++)
         {
-            GetID.Add(Items[i], i);
+            Items[i].ID = i;
             GetItem.Add(i, Items[i]);
         }
     }
 
     public void OnBeforeSerialize()
     {
+        GetItem = new Dictionary<int, ItemData>();
     }
 }
