667681ac2260fbbfefd2f3fe1ee30d2f9eae52cd
Mehmet Berkay Coruk <89832806+MehmetBerkayC@users.noreply.github.com>
2023-11-10 01:43:47 +0300

    Health System works with events as standalone class
    OnDamaged - Triggers when damaged
    OnDead - Triggers when hp below 1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,17 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : HealthSystem
+public class Enemy : MonoBehaviour, IDamageable
 {
+    [SerializeField] int _maxHealth;
     [SerializeField] float _moveSpeed;
     [SerializeField] float _damage;
 
     Transform _player;
-    
+    HealthSystem _healthSystem;
+
     void Start()
     {
         _player = FindObjectOfType<Player>().transform;
+        
+        // Health System
+        _healthSystem = new HealthSystem(_maxHealth);
+        _healthSystem.OnDead += UnitKilled;
     }
 
     void Update()
@@ -19,12 +26,14 @@ public class Enemy : HealthSystem
         transform.position = Vector2.MoveTowards(transform.position, _player.transform.position, _moveSpeed * Time.deltaTime);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void TakeDamage(int damageAmount)
     {
-        if (collision.gameObject.TryGetComponent(out IDamageable enemy))
-        {
-            //Debug.Log("Hit detected, from:" + gameObject.name + " to:" + enemy.gameObject.name);
-            enemy.TakeDamage(_damage);
-        }
+        _healthSystem.TakeDamage(damageAmount);
+    }
+
+    private void UnitKilled(object sender, EventArgs eventArgs)
+    {
+        _healthSystem.OnDead -= UnitKilled;
+        Destroy(this.gameObject);
     }
 }
